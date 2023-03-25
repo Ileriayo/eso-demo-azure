ec8c534c70fc2d57152141e51d52bd546c3e07f0
Adebiyi Imoleayo <cia@Adebiyis-MacBook-Pro.local>
2023-03-25 08:20:45 +0100

    Update secret again

--- secret.tf
+++ secret.tf
@@ -2,7 +2,7 @@
 # Create a Secret in Azure Key Vault
 resource "azurerm_key_vault_secret" "demo_secret" {
   name         = "eso-demo-secret"
-  value        = "Hello Localhost Meetup - This Is A Secret From Azure Key Vault !"
+  value        = "Hello Localhost #LLMeetup23 - This Is A Secret From Azure Key Vault !"
   key_vault_id = azurerm_key_vault.eso-keyvault.id
 
   depends_on = [
